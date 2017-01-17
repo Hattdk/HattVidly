05c7ef66f3a8f048991ab723d4b1e2b53b896f5a
David Hatt <hatt.dk@gmail.com>
2017-01-17 00:22:54 -0800

    second commit from home

---
+++ HattVidly/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HattVidly.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
+
+
+// super git test
