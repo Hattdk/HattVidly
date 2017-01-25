1c7d80c00c513e04d39f0e48c653466abff719c8
David Hatt <hatt.dk@gmail.com>
2017-01-25 00:01:17 -0800

    added membership type

--- HattVidly/Models/Customer.cs
+++ HattVidly/Models/Customer.cs
@@ -10,5 +10,8 @@ namespace HattVidly.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; } // foreign key
+
     }
 }
